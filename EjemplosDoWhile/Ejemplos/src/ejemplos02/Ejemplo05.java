37e858b535b0bdc613a8302c7d4cb53a014abb39
Antonio Ojeda <ajojeda6@utpl.edu.ec>
2024-11-27 11:42:10 -0500

    cambios

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo05.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo05.java
@@ -10,21 +10,24 @@ package ejemplos02;
 /**
  *
  * @author reroes
- */
+ */import java.util.Scanner;
+
 public class Ejemplo05 {
+    
 
     public static void main(String[] args) {
         // 2. numero, i[0, n]
+        Scanner entrada = new Scanner(System.in);
         int numero;
         // 3. potencia, i[0, n]
         int potencia;
         // 6. resultado, i[0, n]
         int resultado;
         // numero <-- 4
-        numero = 4;
-        // 8. potencia <-- 3
-        potencia = 3;
-        // 9. resultado <-- 1
+        System.out.println("Ingrese un numero");
+        numero = entrada.nextInt();
+        System.out.println("Ingrese su potencia");
+        potencia = entrada.nextInt();
         resultado = 1;
 
         int contador = 1;
