55aca46dd2f9d7ab5cac4affd857e9943a7aa1de
Antonio Ojeda <ajojeda6@utpl.edu.ec>
2024-11-27 11:24:02 -0500

    cambios urgentes

--- EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo02.java
+++ EjemplosDoWhile/Ejemplos/src/ejemplos02/Ejemplo02.java
@@ -31,8 +31,17 @@ public class Ejemplo02 {
             cadenaFinal = String.format("%s%.2f\n", cadenaFinal, nota);
             entrada.nextLine(); // limpieza de buffer
 
-            System.out.println("Ingrese (s) si desea salir del ciclo ");
+            System.out.println("Ingrese (s) o (yes)"
+                    + "o (si) o (y) si desea salir del ciclo ");
             salida = entrada.nextLine();
+            switch(salida){
+                case "si":
+                case "s":
+                case "yes":
+                case "y":
+                    bandera = false;
+                    break;
+            }
 
             
             
